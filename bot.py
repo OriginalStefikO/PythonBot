cb6faa10db8e9c1f35aa074e153a5ade382383fb
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2020-09-21 19:49:14 +0200

    Update bot.py
    - added BinToText, TextToBin, HexToText, TextToHex
    - added "help" command
    - edited custom activity

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import discord
+import time
+import math
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Random things"))
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='seznam příkazů "help"'))
         print('Bot je ready')
 
 
@@ -10,6 +12,58 @@ class MyClient(discord.Client):
         # don't respond to ourselves
         if message.author == self.user:
             return
+
+
+        if message.content == "help":
+            embed = discord.Embed(
+                title = "Seznam příkazů:",
+                color = discord.Color.green()
+            )
+            embed.set_footer(
+            text="Made by Stefik_O"
+            )
+            embed.add_field(
+            name="BinToDec", 
+            value="DecToBin",
+            inline= True
+            )
+            embed.add_field(
+            name="BinToHex",
+            value="HexToBin",
+            inline= True
+            )
+            embed.add_field(
+            name="BinToText",
+            value="TextToBin",
+            inline= True
+            )
+            embed.add_field(
+            name="DecToHex",
+            value="HexToDec",
+            inline= True
+            )
+            embed.add_field(
+            name="DecToText",
+            value="TextToDec",
+            inline= True
+            )
+            embed.add_field(
+            name="HexToText",
+            value="TextToHex",
+            inline= True
+            )
+            embed.add_field(
+            name="Encode",
+            value='Pro zakódování musíš zadat dvojciferné číslo, které si musíš zapamatovat pro dekódování:\n Encode 23 Ahoj',
+            inline= False
+            )
+            embed.add_field(
+            name="Decode",
+            value='Pro dekódování musíš použít stejné číslo jako jsi použil na zakódování:\n Decode 23 188729c7c',
+            inline= False
+            )
+
+            await message.channel.send(embed=embed)
 
         if message.content.startswith("BinToDec"):
             input1 = message.content[8:]
@@ -55,5 +109,37 @@ class MyClient(discord.Client):
             await message.channel.send("⇣⇣⇣⇣⇣⇣")
             await message.channel.send(hex(bide).replace("0x",""))
 
+        if message.content.startswith("BinToText"):
+            input1 = message.content[10:]
+            def decode_binary_string(s):
+                return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
+            await message.channel.send("⇣⇣⇣⇣⇣⇣")
+            await message.channel.send(decode_binary_string(input1))
+
+        if message.content.startswith("TextToBin"):
+            input1 = message.content[10:]
+            await message.channel.send("⇣⇣⇣⇣⇣⇣")
+            await message.channel.send(''.join('{:08b}'.format(d) for d in bytearray(input1, 'utf-8')))
+
+        if message.content.startswith("TextToHex"):
+            input1 = message.content[10:]
+            await message.channel.send("⇣⇣⇣⇣⇣⇣")
+            textbin = ''.join('{:08b}'.format(d) for d in bytearray(input1, 'utf-8'))
+            def bindec(n): 
+                return int(n, 2)
+            bide = bindec(textbin)
+            await message.channel.send(hex(bide).replace("0x",""))
+
+        if message.content.startswith("HexToText"):
+            input1 = message.content[10:]
+            await message.channel.send("⇣⇣⇣⇣⇣⇣")
+            vysledek = int(input1, 16)
+            def decbin(n): 
+                return bin(n).replace("0b","")
+            bide = decbin(vysledek)
+            def decode_binary_string(s):
+                return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
+            await message.channel.send(decode_binary_string("0" + bide))
+
 client = MyClient()
-client.run('Token-here')
+client.run('Your token copy here')
